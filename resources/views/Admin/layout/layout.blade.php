d829d3181915915d0e152254a228fef1b7f23fce
Júnior Moreira <junior@adunicamp.org.br>
2021-11-16 15:08:36 -0300

    frontend: Instala o Materialize CSS

--- resources/views/Admin/layout/layout.blade.php
+++ resources/views/Admin/layout/layout.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
     <title>Imóveis Teste</title>
 </head>
 <body>
@@ -14,5 +17,9 @@
     <main class="container">
         @yield('conteudo-principal')
     </main>
+
+   <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
 </body>
 </html>
