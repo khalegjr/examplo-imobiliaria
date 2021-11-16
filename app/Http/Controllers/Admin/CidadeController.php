49c1c1f21faa4ee2ded1ecb8c685f923dd1b4624
Júnior Moreira <junior@adunicamp.org.br>
2021-11-16 16:25:35 -0300

    model: Cria o modelo de Cidades e a chamada no controller

--- app/Http/Controllers/Admin/CidadeController.php
+++ app/Http/Controllers/Admin/CidadeController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cidade;
 use Illuminate\Http\Request;
 
 class CidadeController extends Controller
 {
     public function index()
     {
-        $cidades = ['Campinas', 'São Paulo', 'Salgado', 'Guará'];
+        //$cidades = ['Campinas', 'São Paulo', 'Salgado', 'Guará'];
+        $cidades = Cidade::all();
 
         return view('Admin.Cidade.index')->with('cidades', $cidades);
     }
