daed0c034d97aec0f549ace924366211aa221bb2
Júnior Moreira <junior@adunicamp.org.br>
2021-11-18 16:54:33 -0300

    database: Cria o relacionamento 1-1
    Cria o relacionamento  entre as tabelas  imoveis e enderecos, do tipo 1-1.

--- app/Models/Endereco.php
+++ app/Models/Endereco.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Endereco extends Model
 {
     use HasFactory;
+
+    public function imovel()
+    {
+        /**
+         * Essa é o inverso do relacionamento. O inverso é sempre onde fica a chave primária (chamamos o belongsTo).
+         */
+        return $this->belongsTo(Imovel::class);
+        /**
+         * A convenção do ELoquent para identificar a chave estrangeira nesse caso é pegar o nome do método e acrescentar o sufixo _id. Nesse caso: imovel_id
+         *
+         * Se não segue esse padrão, então, basta adicionar um segundo parâmetro com a chave estrangeira: $this->belongsTo(Imovel::class, 'chave_estrangeira');
+         *
+         * E, se a chave estrangeira não aponta pra primária, acrescentamos um terceiro parâmetro: $this->belongsTo(Imovel::class, 'chave_estrangeira', 'campo_associado');
+         */
+    }
 }
--- app/Models/Imovel.php
+++ app/Models/Imovel.php
@@ -10,4 +10,17 @@ class Imovel extends Model
     use HasFactory;
 
     protected $table = 'imoveis';
+
+    /** Define o relacionamento 1-1 entre endereço e imóvel */
+    public function endereco()
+    {
+        return $this->hasOne(Endereco::class);
+        /**
+         * Por convenção o Eloquent vai procurar na tabela de endereços um campo de chave estrangeira no formato nome_classe_id. No caso seria: imovel_id
+         *
+         * Caso não siga essa convenção devemos indicar manualmente a chave estrangeira. Nesse caso: $this->hasOne(Endereco::class, 'chave_estrangeira');
+         *
+         * Também o Eloquent espera que a chave estrangeira aponte para a chave primária da outra tabela. Caso ela aponte para outro campo que não o primário temos que indicar com um terceiro parâmetro: $this->hasOne(Endereco::class, 'chave_estrangeira', 'camṕo_associado');
+         */
+    }
 }
