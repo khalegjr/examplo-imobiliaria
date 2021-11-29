f6d23e21aa478b2d9a7989348ab537d41e5f83c0
Júnior Moreira <junior@adunicamp.org.br>
2021-11-29 11:15:32 -0300

    Adiciona as mensagens de erro ao formulário

--- resources/views/Admin/Imovel/form.blade.php
+++ resources/views/Admin/Imovel/form.blade.php
@@ -9,6 +9,9 @@
             <div class="input-field">
                 <input type="text" name="titulo" id="titulo">
                 <label for="titulo">Título</label>
+                @error('titulo')
+                    <span class="red-text text-accent-3"><small>{{ $message }}</small></span>
+                @enderror
             </div>
 
             {{-- Cidade --}}
@@ -21,6 +24,9 @@
                     @endforeach
                 </select>
                 <label for="cidade_id">Cídade</label>
+                @error('cidade_id')
+                    <span class="red-text text-accent-3"><small>{{ $message }}</small></span>
+                @enderror
             </div>
 
             {{-- Tipo --}}
@@ -33,6 +39,9 @@
                     @endforeach
                 </select>
                 <label for="tipo_id">Tipo de Imóvel</label>
+                @error('tipo_id')
+                    <span class="red-text text-accent-3"><small>{{ $message }}</small></span>
+                @enderror
             </div>
 
             {{-- Finalidade --}}
@@ -43,8 +52,12 @@
                             <input type="radio" name="finalidade_id" id="finalidade_id" class="with-gap" value="{{ $finalidade->id }}" />
                             <span>{{ $finalidade->nome }}</span>
                         </label>
+                        
                     </span>
                 @endforeach
+                @error('finalidade_id')
+                    <span class="red-text text-accent-3"><small>{{ $message }}</small></span>
+                @enderror
             </div>
 
             {{-- preço dormitórios salas --}}
@@ -52,16 +65,25 @@
                 <div class="input-field col s4">
                     <input type="number" name="preco" id="preco" min="0" step="0.50" />
                     <label for="preco">Preço</label>
+                    @error('preco')
+                        <span class="red-text text-accent-3"><small>{{ $message }}</small></span>
+                    @enderror
                 </div>
 
                 <div class="input-field col s4">
                     <input type="number" name="dormitorios" id="dormitorios" min="0" />
                     <label for="dormitorios">Quantidades de Dormitórios</label>
+                    @error('dormitorios')
+                        <span class="red-text text-accent-3"><small>{{ $message }}</small></span>
+                    @enderror
                 </div>
 
                 <div class="input-field col s4">
                     <input type="number" name="salas" id="salas" min="0" />
                     <label for="salas">Quantidade de Salas</label>
+                    @error('salas')
+                        <span class="red-text text-accent-3"><small>{{ $message }}</small></span>
+                    @enderror
                 </div>
             </div>
 
@@ -70,16 +92,25 @@
                 <div class="input-field col s4">
                     <input type="number" name="terreno" id="terreno" min="0" step="0.50" />
                     <label for="terreno">Terreno em m²</label>
+                    @error('terreno')
+                        <span class="red-text text-accent-3"><small>{{ $message }}</small></span>
+                    @enderror
                 </div>
 
                 <div class="input-field col s4">
                     <input type="number" name="banheiros" id="banheiros" min="0" />
                     <label for="banheiros">Quantidades de Banheiros</label>
+                    @error('banheiros')
+                        <span class="red-text text-accent-3"><small>{{ $message }}</small></span>
+                    @enderror
                 </div>
 
                 <div class="input-field col s4">
                     <input type="number" name="garagens" id="garagens" min="0" />
                     <label for="garagens">Vagas na Garagem</label>
+                    @error('garagens')
+                        <span class="red-text text-accent-3"><small>{{ $message }}</small></span>
+                    @enderror
                 </div>
             </div>
 
@@ -87,6 +118,9 @@
             <div class="input-field">
                 <textarea name="descricao" id="descricao" class="materialize-textarea"></textarea>
                 <label for="descricao">Descrição</label>
+                @error('descricao')
+                    <span class="red-text text-accent-3"><small>{{ $message }}</small></span>
+                @enderror
             </div>
 
             {{-- Endereço --}}
@@ -94,26 +128,41 @@
                 <div class="input-field col s6">
                     <input type="text" name="rua" id="rua" />
                     <label for="rua">Rua</label>
+                    @error('rua')
+                        <span class="red-text text-accent-3"><small>{{ $message }}</small></span>
+                    @enderror
                 </div>
 
                 <div class="input-field col s2">
                     <input type="text" name="numero" id="numero" />
                     <label for="numero">Número</label>
+                    @error('numero')
+                        <span class="red-text text-accent-3"><small>{{ $message }}</small></span>
+                    @enderror
                 </div>
 
                 <div class="input-field col s4">
                     <input type="text" name="complemento" id="complemento" />
                     <label for="complemento">Complemento</label>
+                    @error('complemento')
+                        <span class="red-text text-accent-3"><small>{{ $message }}</small></span>
+                    @enderror
                 </div>
 
                 <div class="input-field col s8">
                     <input type="text" name="bairro" id="bairro" />
                     <label for="bairro">Bairro</label>
+                    @error('bairro')
+                        <span class="red-text text-accent-3"><small>{{ $message }}</small></span>
+                    @enderror
                 </div>
 
                 <div class="input-field col s4">
                     <input type="text" name="cep" id="cep" maxlength="9" />
                     <label for="cep">CEP</label>
+                    @error('cep')
+                        <span class="red-text text-accent-3"><small>{{ $message }}</small></span>
+                    @enderror
                 </div>
             </div>
 
@@ -128,6 +177,9 @@
                         @endforeach
                     </select>
                     <label for="proximidades">Pontos de interesse nas proximidades</label>
+                    @error('proximidades')
+                        <span class="red-text text-accent-3"><small>{{ $message }}</small></span>
+                    @enderror
                 </div>
             </div>
 
