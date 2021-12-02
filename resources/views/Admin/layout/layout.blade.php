2a4c2e8e408677dd24ef9f99c64d5d9b65848228
Júnior Moreira <junior@adunicamp.org.br>
2021-12-02 15:40:49 -0300

    frontend: Cria página de lista de fotos de imóvel

--- resources/views/Admin/layout/layout.blade.php
+++ resources/views/Admin/layout/layout.blade.php
@@ -11,6 +11,8 @@
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
+    <link rel="stylesheet" href="{{ asset('css/fotos.css') }}">
+
     <title>Imóveis Teste</title>
 </head>
 <body>
