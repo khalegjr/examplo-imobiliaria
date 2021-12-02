3682418b04ec10c447c8352ccf2c0b59cdf0069e
Júnior Moreira <junior@adunicamp.org.br>
2021-12-02 15:41:33 -0300

    backend: Cria controller de fotos

--- app/Http/Controllers/Admin/FotoController.php
+++ app/Http/Controllers/Admin/FotoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Foto;
 use App\Models\Imovel;
 use Illuminate\Http\Request;
 
@@ -17,7 +18,9 @@ class FotoController extends Controller
     {
         $imovel = Imovel::findOrFail($id);
 
-        return view('Admin.Imovel.fotos.index', compact('imovel'));
+        $fotos = Foto::where('imovel_id', $id)->get();
+
+        return view('Admin.Imovel.fotos.index', compact('imovel', 'fotos'));
     }
 
     /**
@@ -25,9 +28,11 @@ class FotoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create($id)
     {
-        //
+        $imovel = Imovel::findOrFail($id);
+
+        return view('Admin.Imovel.fotos.form', compact('imovel'));
     }
 
     /**
@@ -36,9 +41,21 @@ class FotoController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, $idImovel)
     {
-        //
+        // checar se veio a imagem na requisição e se não houve erro no upload
+        if ($request->hasFile('foto') && $request->foto->isValid()) {
+            $fotoURL = $request->foto->store("imovel/$idImovel", 'public');
+
+            // Gravando a URL da foto no banco
+            $foto = new Foto();
+            $foto->url = $fotoURL;
+            $foto->imovel_id = $idImovel;
+            $foto->save();
+        }
+
+        $request->session()->flash('sucesso', 'Foto incluída com sucesso!');
+        return redirect()->route('admin.imoveis.fotos.index', $idImovel);
     }
 
     /**
